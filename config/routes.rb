0bf1e8242835386e135f92bb3704c68c5cf5c2e7
Penciler <eraser41@livemail.tw>
2018-04-04 11:51:33 +0800

    add basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,26 @@ Rails.application.routes.draw do
   devise_for :users
 
   # 請依照專案指定規格來設定路由
+  root "tweets#index"
+  resources :tweets, only: [:create, :index] do
 
+    resources :replies, only: [:create, :index]
+  end
+
+  resources :users, only: [:edit] do
+    member do
+      get :tweets
+      get :followings
+      get :followers
+      get :likes
+    end
+  end
+
+  resources :followships, only: [:create, :destroy]
+
+  namespace :admin do
+    resources :tweets, only: [:index, :destroy]
+    resources :users, only: [:index]
+  end
+  
 end
